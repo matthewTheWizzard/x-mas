239f432f76067b3907880f7d2a15bee1684ca02c
matthewthewizzard <mmdrumnew@gmail.com>
2024-12-24 08:58:37 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Edit } from './pages/edit/edit';
 export const Navigation = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef(new Audio('/xmas.mp3')); // Создаём единственный экземпляр Audio
-  audioRef.current.volume = 0.15;
+  audioRef.current.volume = 0.10;
 
   const togglePlay = () => {
     const audio = audioRef.current;
